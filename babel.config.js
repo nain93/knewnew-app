e9db51e44b6303944f11ef3bb7eb8a6b1e53afbb
nain93 <rnrb555@gmail.com>
2022-04-28 17:26:15 +0900

    절대경로 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: [
+					'.ios.ts',
+					'.android.ts',
+					'.ts',
+					'.ios.tsx',
+					'.android.tsx',
+					'.tsx',
+					'.jsx',
+					'.js',
+					'.json',
+				],
+				alias: {
+					'~': './src',
+					'@components': './src/components',
+					'@navigators': './src/navigators',
+					'@api': './src/api',
+					'@screens': './src/screens',
+					'@assets': './src/assets',
+					'@styles': './src/styles',
+					'@types': './src/types',
+					'@utils': './src/utils'
+				},
+			},
+		],
+	],
 };
