bb24db5661bad2c89a2f2ba129231435dc9cc405
salybu <rkdmsdl93@gmail.com>
2022-05-06 18:53:53 +0900

    커밋 수정

---
+++ src/components/icon/reviewIcon.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Image, ViewStyle } from "react-native";
+import { heart, triangle, close } from "~/assets/icons";
+
+interface ReviewIconProp {
+    review: string;
+}
+
+const ReviewIcon = ({ review }: ReviewIconProp) => {
+    return (
+        <Image
+            source={imgSource(review)}
+            resizeMode="contain"
+            style={{ width: 20, height: 20 }}
+        />
+    );
+};
+
+const imgSource = (review: string) => {
+    switch (review) {
+        case "heart":
+            return heart;
+        case "triangle":
+            return triangle;
+        case "close":
+            return close;
+    }
+}
+
+export default ReviewIcon;
