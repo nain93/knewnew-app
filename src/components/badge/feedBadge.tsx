6013e89eb3fdd6f67548c474804282102cfaba15
salybu <rkdmsdl93@gmail.com>
2022-05-09 16:44:43 +0900

    전체 피드 화면 작업

--- src/components/badge/feedBadge.tsx
+++ src/components/badge/feedBadge.tsx
@@ -1,30 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, ViewStyle } from "react-native";
+import { StyleSheet, Text, View, ViewStyle } from "react-native";
 import theme from "~/styles/theme";
 import { d2p } from "~/utils";
 
 interface FeedBadgeProp {
-    text: string,
-    viewStyle?: ViewStyle
+  text: string,
 }
 
-const FeedBadge = ({ text, viewStyle }: FeedBadgeProp) => {
-    return (
-        <TouchableOpacity style={styles.badge}>
-            <Text style={styles.badgeText}>{text}</Text>
-        </TouchableOpacity>);
+const FeedBadge = ({ text }: FeedBadgeProp) => {
+  return (
+    <View style={styles.badge}>
+      <Text style={styles.badgeText}>{text}</Text>
+    </View>);
 };
 
 export default FeedBadge;
 
 const styles = StyleSheet.create({
-    badge: {
-        borderWidth: 1, borderColor: theme.color.main,
-        borderRadius: 9,
-        paddingVertical: d2p(1), paddingHorizontal: d2p(4),
-        marginRight: 'auto'
-    },
-    badgeText: {
-        fontSize: 10, color: theme.color.main
-    }
+  badge: {
+    borderWidth: 1, borderColor: theme.color.main,
+    borderRadius: 9,
+    paddingVertical: d2p(1), paddingHorizontal: d2p(4),
+    marginRight: 'auto'
+  },
+  badgeText: {
+    fontSize: 10, color: theme.color.main
+  }
 });
