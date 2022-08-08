088f84a08da080308d2df35499c53fd4e8c8a149
nain93 <rnrb555@gmail.com>
2022-08-08 16:03:27 +0900

    게시글에 ...더보기> 추가

--- src/components/style/CustomText.tsx
+++ src/components/style/CustomText.tsx
@@ -1,14 +1,18 @@
 import { StyleProp, Text, TextStyle } from 'react-native';
 import React from 'react';
+import ReadMore from '@fawazahmed/react-native-read-more';
 
 interface CustomTextProps {
   children: React.ReactNode
   style?: StyleProp<TextStyle>
+  numberOfLines?: number
 }
 
-const CustomText = ({ children, style }: CustomTextProps) => {
+const CustomText = ({ children, style, numberOfLines }: CustomTextProps) => {
   return (
-    <Text style={[{ fontSize: 15 }, style]}>{children}</Text>
+    <Text
+      numberOfLines={numberOfLines}
+      style={[{ fontSize: 15 }, style]}>{children}</Text>
   );
 };
 
